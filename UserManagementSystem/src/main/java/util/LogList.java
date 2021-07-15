b676631055e1cbcabbe2b48a8ff6132c03cd0ba6
Adam The Creator <78386606+adamthecreator369@users.noreply.github.com>
2021-07-15 11:57:35 -0500

    Add files via upload
    Bug fixes. Unwanted actions were being allowed.

---
+++ UserManagementSystem/src/main/java/util/LogList.java
@@ -0,0 +1,70 @@
+package main.java.util;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import main.java.entities.LogEvent;
+import main.java.entities.User;
+
+public class LogList {
+	
+	// Data field
+	private ArrayList<LogEvent> logEvents;
+	
+	// Constructor
+	public LogList() {
+		logEvents = new ArrayList<>();
+	}
+	
+	/** Adds an item to the LogList */
+	public void add(LogEvent event) {
+		logEvents.add(event);
+	}
+	
+	/**
+	 * Gets and returns all log activity for a particular user.
+	 * @param user: the user whose activity is being retrieved.
+	 * @return: the users log activity.
+	 */
+	public String getUserActivity(User user) {
+		StringBuilder sb = new StringBuilder();
+		for (LogEvent event : logEvents) {
+			if (event.compareTo(user)) {
+				sb.append(event);
+			}
+		}
+		return sb.toString();
+	}
+	
+	/** 
+	 * Creates and returns the LogList in String format.
+	 * @return: the LogList in String format.
+	 */
+	public String displayList() {
+		StringBuilder sb = new StringBuilder();
+		for (LogEvent event : logEvents) {
+			sb.append(event);
+		}
+		return sb.toString();
+	}
+	
+	@Override 
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (LogEvent event : logEvents) {
+			sb.append(event.toFileString());
+		}
+		return sb.toString();
+	}
+	
+	/** Prints the LogList in its current state to an output file */
+	public void print() throws IOException {
+		FileOutputStream fout = new FileOutputStream("log.txt");
+		PrintWriter writer = new PrintWriter(fout);
+		writer.print(toString());
+		writer.close();
+		fout.close();
+	}
+}
